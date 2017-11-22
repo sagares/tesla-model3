7edb45b75848404c3deb7d57b862df1532a0c5d1
sagares <santosh.sagare@cdk.com>
2017-11-23 01:03:52 +0530

    Form validation added

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -5,9 +5,19 @@ function mainController($scope, $interval){
 	$scope.message = 'Introducing the Model 3';
 	$scope.formText = 'Be the first to own the new Tesla Model 3. Sing up to our mailing liat to find out more.';
 	$scope.countries = ["USA", "Canada", "India", "Australia", "England", "France"];
-	$scope.selectedCoutry = '';
 
-	$scope.subscribe = function(){
-		console.log('submitted');
+	$scope.user = {};
+	$scope.user.country = '';
+
+	$scope.subscribe = function(valid){
+		if(valid) {
+			var user = $scope.user;
+			console.log("User info submitted: ");
+			console.log(user.firstName+ " " + user.lastName);
+			console.log(user.country);
+			console.log(user.email);
+		} else {
+			console.log('Some fields missing');
+		}
 	}
 }
